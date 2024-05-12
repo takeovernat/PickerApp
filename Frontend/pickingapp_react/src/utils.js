455b9726e3b2b9e5dca83fce2b3b2316c7750ea6
takeovernat <natnaelteshome9@gmail.com>
2024-05-11 22:01:13 -0500

    add utils file

---
+++ Frontend/pickingapp_react/src/utils.js
@@ -0,0 +1,23 @@
+export async function makeUpdateCall(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "PUT", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: data, // body data type must match "Content-Type" header
+  });
+  //console.log(response.json());
+  return response.json();
+}
+
+export async function fetchLinesbyOrder(orderNum) {
+  let res = await fetch(`http://localhost:8000/linefromorder/${orderNum}`);
+  res = await res.json();
+  return res;
+}
